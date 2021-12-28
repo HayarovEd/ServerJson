eda9b3ba24cbf47ecd9e4da8a22a6d32af881081
HayarovEd <edurd@rambler.ru>
2021-12-28 06:10:36 +0500

    server

--- src/main/java/StartForm.java
+++ src/main/java/StartForm.java
@@ -1,2 +1,41 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class StartForm {
+    @FXML
+    private Label levelView;
+    ReleaseServer releaseServer = new ReleaseServer();
+    @FXML
+    private   Button startBottom;
+    @FXML
+    private Button stopBottom;
+    @FXML
+    public void initialize(){
+        levelView.setText(levelView.getText()+"Server is started");
+        startBottom.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            try {
+                releaseServer.startServer();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        stopBottom.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            try {
+                releaseServer.stopServer();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+    }
+
+
+
 }
---
+++ src/main/java/initServer.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class initServer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("StartForm.fxml")));
+
+        primaryStage.setScene(new Scene(root, primaryStage.getWidth(), primaryStage.getHeight()));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
